10b571c71ec19882a389c94d055892c72e3a0e10
Adam Chmelynski <adam.chmelynski@gmail.com>
2015-07-29 12:50:38 -0400

    a

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,7 +11,8 @@
 import os
 
 #developmentServer = (os.getcwd() == '/cygdrive/c/Users/Adam/Desktop/frce/mysite')
-developmentServer = True
+#developmentServer = (os.getcwd() == '/cygdrive/c/Users/Adam/Desktop/frce/mysite')
+developmentServer = (os.getcwd() != '/app');
 
 if developmentServer:
 	DEBUG = True
@@ -34,27 +35,26 @@ MANAGERS = ADMINS
 # how do we get different apps to use the different databases below?  right now, they all use 'default', and i don't know how to change it
 # file:///C:/Users/Adam/Desktop/django-docs-1.5-en/topics/db/multi-db.html
 
-DATABASES = {
-	#'default' : {
-	#	'ENGINE' : 'django.db.backends.postgresql_psycopg2' ,
-	#	'NAME' : 'griddl' ,
-	#	
-	#	#'USER' : '' ,
-	#	#'PASSWORD' : '' ,
-	#	#'HOST' : '127.0.0.1' ,
-	#	#'PORT' : '' ,
-	#	
-	#	'USER' : env['DOTCLOUD_DB_SQL_LOGIN'],
-	#	'PASSWORD' : env['DOTCLOUD_DB_SQL_PASSWORD'],
-	#	'HOST' : env['DOTCLOUD_DB_SQL_HOST'],
-	#	'PORT' : int(env['DOTCLOUD_DB_SQL_PORT'])
-	#} ,
-	'default' : {
-		'ENGINE' : 'django.db.backends.sqlite3' , 
-		#'NAME' : 'C:\\Users\\Adam\\Desktop\\frce\\mysite\\db.sqlite3'
-		'NAME' : 'C:/cygwin64/home/adam/frce/mysite/db.sqlite3'
+if developmentServer:
+	DATABASES = {
+		'default' : {
+			'ENGINE' : 'django.db.backends.sqlite3' , 
+			'NAME' : 'C:/cygwin64/home/adam/frce/mysite/db.sqlite3'
+		}
 	}
-}
+else:
+	import dj_database_url
+	DATABASES['default'] =  dj_database_url.config()
+	#DATABASES = {
+	#	'default' : {
+	#		'ENGINE' : 'django.db.backends.postgresql_psycopg2' ,
+	#		'NAME' : 'griddl' ,
+	#		'USER' : env['DOTCLOUD_DB_SQL_LOGIN'],
+	#		'PASSWORD' : env['DOTCLOUD_DB_SQL_PASSWORD'],
+	#		'HOST' : env['DOTCLOUD_DB_SQL_HOST'],
+	#		'PORT' : int(env['DOTCLOUD_DB_SQL_PORT'])
+	#	}
+	#}
 
 #DATABASE_ROUTERS = [ 'path.to.AuthRouter' , 'path.to.MasterSlaveRouter' ]
 
@@ -94,19 +94,27 @@ MEDIA_URL = ''
 #	STATIC_ROOT = '/home/dotcloud/volatile/static/'
 
 # not sure what heroku demands for this
-STATIC_ROOT = ''
+
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
-STATIC_URL = '/static/'
-
 # Additional locations of static files
-STATICFILES_DIRS = (
-	# Put strings here, like "/home/html/static" or "C:/www/django/static".
-	# Always use forward slashes, even on Windows.
-	# Don't forget to use absolute paths, not relative paths.
-	#'c:/users/adam/desktop/frce/mysite/griddl/static' #will this break a production push?
-)
+# Put strings here, like "/home/html/static" or "C:/www/django/static".
+# Always use forward slashes, even on Windows.
+# Don't forget to use absolute paths, not relative paths.
+#'c:/users/adam/desktop/frce/mysite/griddl/static' #will this break a production push?
+
+if developmentServer:
+	STATIC_ROOT = ''
+	STATIC_URL = '/static/'
+	STATICFILES_DIRS = ()
+else:
+	BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+	STATIC_ROOT = 'staticfiles'
+	STATIC_URL = '/static/'
+	STATICFILES_DIRS = ( os.path.join(BASE_DIR, 'static') )
+
+
 
 # List of finder classes that know how to find static files in
 # various locations.
