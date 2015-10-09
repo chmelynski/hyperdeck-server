88ac8f2a40be23d9ddcbedae1f54b903e710da6d
Noah Hall <noah.t.hall@gmail.com>
2015-10-09 19:25:09 +0000

    add staticfiles handler to urls.py

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,5 +1,5 @@
-
 from django.conf.urls import patterns, include, url
+from mysite import settings
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -15,6 +15,7 @@ urlpatterns = patterns('',
 	
 	# Uncomment the next line to enable the admin:
 	url(r'^admin/', include(admin.site.urls)),
+    url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
 	url(r'', include('griddl.urls'))
 	#url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 )
