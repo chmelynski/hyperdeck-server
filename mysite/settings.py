619373adf1d2f097f4e52b009f133c80376d08d6
Noah Hall <noah.t.hall@gmail.com>
2015-10-15 19:25:21 +0000

    more logging-related changes

--- mysite/settings.py
+++ mysite/settings.py
@@ -198,9 +198,14 @@ SESSION_SERIALIZER = 'django.contrib.sessions.serializers.JSONSerializer'
 #	]
 #}
 
-# A sample logging configuration.
-# The only tangible logging performed by this configuration is to send an email to the site admins on every HTTP 500 error when DEBUG=False.
 # See http://docs.djangoproject.com/en/dev/topics/logging for more details on how to customize your logging configuration.
+
+# this is quickly going to force us to adopt a mutiple-settings-file approach or risk madness, maybe both
+if noahDev or developmentServer:
+    default_handler = 'file'
+else:
+    default_handler = 'stdout'
+
 LOGGING = {
 	'version': 1,
 	'disable_existing_loggers': False,
@@ -209,27 +214,33 @@ LOGGING = {
 			'()': 'django.utils.log.RequireDebugFalse'
 		}
 	},
-  'formatters': {
-    'verbose': {
-      'format' : "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
-      'datefmt' : "%d/%b/%Y %H:%M:%S"
-    },
-    'simple': {
-      'format': '%(levelname)s - %(message)s'
-    },
-  },
+        'formatters': {
+                'verbose': {
+                    'format' : "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
+                    'datefmt' : "%d/%b/%Y %H:%M:%S"
+                },
+                'simple': {
+                    'format': '%(levelname)s - %(message)s'
+                },
+        },
 	'handlers': {
 		'mail_admins': {
 			'level': 'ERROR',
 			'filters': ['require_debug_false'],
 			'class': 'django.utils.log.AdminEmailHandler'
 		},
-    'billing': {
-      'level': 'DEBUG',
-      'class': 'logging.FileHandler',
-      'filename': BASE_DIR + '/billing/billing.log',
-      'formatter': 'verbose'
-    }
+                'file': {
+                    'level': 'DEBUG',
+                    'class': 'logging.FileHandler',
+                    'filename': BASE_DIR + '/app.log',
+                    'formatter': 'verbose'
+                },
+                'stdout': {
+                    'level': 'DEBUG',
+                    'class': 'logging.StreamHandler',
+                    'formatter': 'verbose',
+
+                }
 	},
 	'loggers': {
 		'django.request': {
@@ -237,11 +248,11 @@ LOGGING = {
 			'level': 'ERROR',
 			'propagate': True,
 		},
-    'billing': {
-      'handlers': ['billing'],
-      'level': 'DEBUG',
-      'propagate': False,
-    }
+                'billing': {
+                    'handlers': [default_handler],
+                    'level': 'DEBUG',
+                    'propagate': False,
+                }
 	}
 }
 
