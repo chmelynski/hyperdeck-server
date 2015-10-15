81fa9bc5260634f5f89d047f98b93178bedb4756
Noah Hall <noah.t.hall@gmail.com>
2015-10-15 19:56:31 +0000

    durrr replace placeholders with returning a response

--- billing/views.py
+++ billing/views.py
@@ -5,19 +5,19 @@ logger = logging.getLogger(__name__)
 def activate(request):
         '''FastSpring Notifications endpoint -- Subscription Activation'''
         logger.debug(request.GET)
-        pass
+        return HttpResponse()
 
 def change(request):
         '''FastSpring Notifications endpoint -- Subscription Change'''
         logger.debug(request.GET)
-        pass
+        return HttpResponse()
 
 def deactivate(request):
         '''FastSpring Notifications endpoint -- Subscription Deactivation'''
         logger.debug(request.GET)
-        pass
+        return HttpResponse()
 
 def payfail(request):
         '''FastSpring Notifications endpoint -- Subscription Payment Failure'''
         logger.debug(request.GET)
-        pass
+        return HttpResponse()
