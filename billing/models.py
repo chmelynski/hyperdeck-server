404f27a438e29c29ebe6304d09fe8778870a1cfb
Noah Hall <noah.t.hall@gmail.com>
2015-10-22 20:58:09 +0000

    updated billing models to include Plan

--- billing/models.py
+++ billing/models.py
@@ -1,29 +1,68 @@
+'''
+billing models
+'''
+
 from django.db import models
+
+
+class Plan(models.Model):
+    '''
+    Details about each plan available
+    Unfortunately, tied pretty heavily to FastSpring settings.
+
+    NB: table contents managed by billing/fixtures/initial_data.json
+    '''
+
+    FREE, SMALL, MEDIUM, LARGE = range(0, 3)
+
+    NAMES = (
+        (FREE, 'Free'),
+        (SMALL, 'Small'),
+        (MEDIUM, 'Medium'),
+        (LARGE, 'Large')
+    )
+
+    # in MB
+    # note: changes here *require* changes in FastSpring settings,
+    #       and vice versa.
+    SIZES = (
+        (FREE, 1),
+        (SMALL, 5),
+        (MEDIUM, 25),
+        (LARGE, 250)
+    )
+
+    name = models.IntegerField(choices=NAMES, default=FREE)
+
+    def _get_size(self):
+        return self.SIZES[self.name]
+
+    def _set_size(self):
+        self.size = self.SIZES[self.name]
+
+    size = property(_get_size, _set_size)
+
+    def __unicode__(self):
+        return "%s Plan (%dMB)".format(self.get_name_display(), self.size)
+
 
 class Subscription(models.Model):
     '''
     Holds current account status & external reference ID from FastSpring
     '''
 
-    PLAN_SIZES = (
-            ('S', 'small'),
-            ('M', 'medium'),
-            ('L', 'large'),
-            ('XL', 'whoa')
-    )
-
-    reference_id = models.CharField(max_length=255, 
+    reference_id = models.CharField(max_length=255,
                                     help_text="FastSpring reference ID")
     status = models.CharField(max_length=8)
     status_reason = models.CharField(max_length=20)
-    details_url = models.CharField(max_length=255, 
+    details_url = models.CharField(max_length=255,
                                    help_text="FastSpring details link")
-    description = models.CharField(max_length=255, 
+    description = models.CharField(max_length=255,
                                    help_text="FS plan description, \
                                                 e.g. '$10 monthly'")
-    
-    plan = models.CharField(max_length=2, choices=PLAN_SIZES, default='S')
+
+    plan = models.ForeignKey(Plan, to_field='name', default=Plan.FREE)
 
     def __unicode__(self):
         return "Ref#: " + self.reference_id + \
-            " (%s)".format(self.get_plan_display())
+            " (%s)".format(self.plan.get_name_display())
