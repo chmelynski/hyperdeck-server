eb785bec97405f113d18461e952dd35566d8e192
Noah Hall <noah.t.hall@gmail.com>
2015-10-23 20:38:11 +0000

    big changeset:
      - several files now PEP8 compliant or v close
      - more template work for prettier forms
      - couple of bootstrap template helper modules

--- griddl/urls.py
+++ griddl/urls.py
@@ -1,52 +1,52 @@
-
-from django.conf.urls import patterns, include, url
+from django.conf.urls import patterns, url
 
 from griddl import views
 
-urlpatterns = patterns('',
-	url(r'^$', views.index, name='index'),                         # ex: /
-	url(r'^filereport$', views.filereport, name='filereport'),     # ex: /filereport
-	url(r'^drawing$', views.drawing, name='drawing'),              # ex: /drawing?text=foo.txt&table=bar.tsv
-	url(r'^music$', views.music, name='music'),                    # ex: /music?song=gymnopedie
-	url(r'^scrubber$', views.scrubber, name='scrubber'),           # ex: /scrubber?type=rawjson&json=foo.json
-	url(r'^image$', views.image, name='image'),                    # ex: /image
-	
-	url(r'^password_change$', 'django.contrib.auth.views.password_change'),
-	url(r'^password_change_done$', 'django.contrib.auth.views.password_change_done'),
-	url(r'^password_reset$', 'django.contrib.auth.views.password_reset'),
-	url(r'^password_reset_done$', 'django.contrib.auth.views.password_reset_done'),
-	url(r'^password_reset_confirm$', 'django.contrib.auth.views.password_reset_confirm'),
-	url(r'^password_reset_complete$', 'django.contrib.auth.views.password_reset_complete'),
-	
-	url(r'^login$', 'django.contrib.auth.views.login'),
-	url(r'^ajaxlogin$', views.ajaxlogin),
-	url(r'^logout$', views.logoutView),
-	url(r'^saveasForm$', views.saveasForm),
-	url(r'^newcsrftoken$', views.newcsrftoken),
-	url(r'^signup$', views.signup),
-	url(r'^join$', views.join),
-	url(r'^ajaxjoin$', views.ajaxjoin),
-	
-	
-	#url(r'^f/(?P<userid>[0-9]+)/(?P<bookid>[A-Za-z0-9-]+)$', views.workbook, name='workbook'),
-	url(r'^f/(?P<userid>[0-9]+)/(?P<path>[A-Za-z0-9-/ ]*)$', views.workbook2),
-	url(r'^d/(?P<userid>[0-9]+)/(?P<path>[A-Za-z0-9-/]*)$', views.directory),
-	
-	url(r'^accounts/profile/$', views.profileRedirect),
-	url(r'^editProfile$', views.editProfile),
-	url(r'^(?P<userid>[0-9]+)$', views.profile),
-	url(r'^(?P<userid>[0-9]+)/profile$', views.editProfile),
-	
-	#url(r'^save/(?P<bookid>[A-Za-z0-9-]+)$', views.save),
-	url(r'^save$', views.save),
-	url(r'^saveas$', views.saveas),
-	url(r'^create$', views.create),
-	url(r'^createDir$', views.createDir),
-	url(r'^rename$', views.rename),
-	url(r'^delete$', views.delete),
-	url(r'^move$', views.move),
-	url(r'^togglepublic$', views.togglepublic),
-	
-	#url(r'^(?P<username>[A-Za-z\-]+)', views.path, name='path'),  # ex: /chmelynski/path/to/workbook
+urlpatterns = patterns(
+    '',
+    url(r'^$', views.index, name='index'),
+    url(r'^filereport$', views.filereport, name='filereport'),     # ex: /filereport
+    url(r'^drawing$', views.drawing, name='drawing'),              # ex: /drawing?text=foo.txt&table=bar.tsv
+    url(r'^music$', views.music, name='music'),                    # ex: /music?song=gymnopedie
+    url(r'^scrubber$', views.scrubber, name='scrubber'),           # ex: /scrubber?type=rawjson&json=foo.json
+    url(r'^image$', views.image, name='image'),                    # ex: /image
 
+    url(r'^ajaxlogin$', views.ajaxlogin),
+    url(r'^logout$', views.logoutView),
+    url(r'^saveasForm$', views.saveasForm),
+    url(r'^newcsrftoken$', views.newcsrftoken),
+    url(r'^signup$', views.signup),
+    url(r'^register$', views.register),
+    url(r'^ajaxregister$', views.ajaxregister),
+
+    # url(r'^f/(?P<userid>[0-9]+)/(?P<bookid>[A-Za-z0-9-]+)$', views.workbook, name='workbook'),
+    url(r'^f/(?P<userid>[0-9]+)/(?P<path>[A-Za-z0-9-/ ]*)$', views.workbook2),
+    url(r'^d/(?P<userid>[0-9]+)/(?P<path>[A-Za-z0-9-/]*)$', views.directory),
+
+    url(r'^accounts/profile/$', views.profileRedirect),
+    url(r'^editProfile$', views.editProfile),
+    url(r'^(?P<userid>[0-9]+)$', views.profile),
+    url(r'^(?P<userid>[0-9]+)/profile$', views.editProfile),
+
+    # url(r'^save/(?P<bookid>[A-Za-z0-9-]+)$', views.save),
+    url(r'^save$', views.save),
+    url(r'^saveas$', views.saveas),
+    url(r'^create$', views.create),
+    url(r'^createDir$', views.createDir),
+    url(r'^rename$', views.rename),
+    url(r'^delete$', views.delete),
+    url(r'^move$', views.move),
+    url(r'^togglepublic$', views.togglepublic),
+)
+
+# builtin auth views
+urlpatterns += patterns(
+    'django.contrib.auth.views',
+    url(r'^password_change$', 'password_change'),
+    url(r'^password_change_done$', 'password_change_done'),
+    url(r'^password_reset$', 'password_reset'),
+    url(r'^password_reset_done$', 'password_reset_done'),
+    url(r'^password_reset_confirm$', 'password_reset_confirm'),
+    url(r'^password_reset_complete$', 'password_reset_complete'),
+    url(r'^login$', 'login')
 )
--- mysite/settings.py
+++ mysite/settings.py
@@ -1,30 +1,29 @@
-# we can use os.getcwd() to dynamically distinguigh between development and production
+# we can use os.getcwd() to distinguigh between development and production
 # in development, os.getcwd() => '/cygdrive/c/Users/Adam/Desktop/frce/mysite'
 # i don't know what it is in production, but it ain't that
 import os
 
-#developmentServer = (os.getcwd() == '/cygdrive/c/Users/Adam/Desktop/frce/mysite')
-developmentServer = (os.getcwd() != '/app');
+developmentServer = (os.getcwd() != '/app')
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-noahDev = os.getenv('griddlDev');
+noahDev = os.getenv('griddlDev')
 if noahDev:
     developmentServer = False
 
 if developmentServer:
-	DEBUG = True
-	TEMPLATE_DEBUG = DEBUG
+    DEBUG = True
+    TEMPLATE_DEBUG = DEBUG
 elif noahDev:
-	DEBUG = True
-	TEMPLATE_DEBUG = DEBUG
+    DEBUG = True
+    TEMPLATE_DEBUG = DEBUG
 else:
-	DEBUG = True
-	TEMPLATE_DEBUG = DEBUG
+    DEBUG = True
+    TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
-	# ('Your Name', 'your_email@example.com'),
-        ("Noah Hall", "noah.t.hall@gmail.com")
+    # ('Your Name', 'your_email@example.com'),
+    ("Noah Hall", "noah.t.hall@gmail.com")
 )
 
 MANAGERS = ADMINS
@@ -34,39 +33,37 @@ MANAGERS = ADMINS
 # HOST : Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP
 # PORT : Set to empty string for default
 
-# how do we get different apps to use the different databases below?  right now, they all use 'default', and i don't know how to change it
-# file:///C:/Users/Adam/Desktop/django-docs-1.5-en/topics/db/multi-db.html
 
 if developmentServer:
-	DATABASES = {
-		'default' : {
-			'ENGINE' : 'django.db.backends.sqlite3' , 
-			'NAME' : 'C:/cygwin64/home/adam/frce/mysite/db.sqlite3'
-		}
-	}
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': 'C:/cygwin64/home/adam/frce/mysite/db.sqlite3'
+        }
+    }
 elif noahDev:
-	DATABASES = {
-		'default' : {
-			'ENGINE' : 'django.db.backends.sqlite3' , 
-			'NAME' : '/var/www/griddl/db.sqlite3'
-		}
-	}
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': '/var/www/griddl/db.sqlite3'
+        }
+    }
 
 else:
-	import dj_database_url
-	DATABASES = { 'default' : dj_database_url.config() }
-	#DATABASES = {
-	#	'default' : {
-	#		'ENGINE' : 'django.db.backends.postgresql_psycopg2' ,
-	#		'NAME' : 'griddl' ,
-	#		'USER' : env['DOTCLOUD_DB_SQL_LOGIN'],
-	#		'PASSWORD' : env['DOTCLOUD_DB_SQL_PASSWORD'],
-	#		'HOST' : env['DOTCLOUD_DB_SQL_HOST'],
-	#		'PORT' : int(env['DOTCLOUD_DB_SQL_PORT'])
-	#	}
-	#}
+    import dj_database_url
+    DATABASES = {'default': dj_database_url.config()}
+    # DATABASES = {
+    #    'default' : {
+    #        'ENGINE' : 'django.db.backends.postgresql_psycopg2' ,
+    #        'NAME' : 'griddl' ,
+    #        'USER' : env['DOTCLOUD_DB_SQL_LOGIN'],
+    #        'PASSWORD' : env['DOTCLOUD_DB_SQL_PASSWORD'],
+    #        'HOST' : env['DOTCLOUD_DB_SQL_HOST'],
+    #        'PORT' : int(env['DOTCLOUD_DB_SQL_PORT'])
+    #    }
+    # }
 
-#DATABASE_ROUTERS = [ 'path.to.AuthRouter' , 'path.to.MasterSlaveRouter' ]
+# DATABASE_ROUTERS = [ 'path.to.AuthRouter' , 'path.to.MasterSlaveRouter' ]
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
@@ -78,19 +75,24 @@ ALLOWED_HOSTS = []
 # In a Windows environment this must be set to your system time zone.
 TIME_ZONE = 'America/New_York'
 
-# Language code for this installation. All choices can be found here: http://www.i18nguy.com/unicode/language-identifiers.html
+# Language code for this installation. All choices can be found here:
+# http://www.i18nguy.com/unicode/language-identifiers.html
 LANGUAGE_CODE = 'en-us'
 # ??
 SITE_ID = 1
-# If you set this to False, Django will make some optimizations so as not to load the internationalization machinery.
+# If you set this to False, Django will make some optimizations so as not to
+# load the internationalization machinery.
 USE_I18N = True
-# If you set this to False, Django will not format dates, numbers and calendars according to the current locale.
+# If you set this to False, Django will not format dates, numbers and calendars
+# according to the current locale.
 USE_L10N = True
 # If you set this to False, Django will not use timezone-aware datetimes.
 USE_TZ = True
-# Absolute filesystem path to the directory that will hold user-uploaded files. Example: "/var/www/example.com/media/"
+# Absolute filesystem path to the directory that will hold user-uploaded files.
+# Example: "/var/www/example.com/media/"
 MEDIA_ROOT = ''
-# URL that handles the media served from MEDIA_ROOT. Make sure to use a trailing slash. Examples: "http://example.com/media/", "http://media.example.com/"
+# URL that handles the media served from MEDIA_ROOT. Make sure to use a
+# trailing slash. Examples: "http://example.com/media/"
 MEDIA_URL = ''
 
 # Absolute path to the directory static files should be collected to.
@@ -98,10 +100,10 @@ MEDIA_URL = ''
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
 
-#if developmentServer:
-#	STATIC_ROOT = ''
-#else:
-#	STATIC_ROOT = '/home/dotcloud/volatile/static/'
+# if developmentServer:
+#    STATIC_ROOT = ''
+# else:
+#    STATIC_ROOT = '/home/dotcloud/volatile/static/'
 
 # not sure what heroku demands for this
 
@@ -112,26 +114,24 @@ MEDIA_URL = ''
 # Put strings here, like "/home/html/static" or "C:/www/django/static".
 # Always use forward slashes, even on Windows.
 # Don't forget to use absolute paths, not relative paths.
-#'c:/users/adam/desktop/frce/mysite/griddl/static' #will this break a production push?
 
 if developmentServer:
-	STATIC_ROOT = ''
-	STATIC_URL = '/static/'
-	STATICFILES_DIRS = ()
+    STATIC_ROOT = ''
+    STATIC_URL = '/static/'
+    STATICFILES_DIRS = ()
 else:
-	#STATIC_ROOT = '/app/griddl/static'
-	STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-	STATIC_URL = '/static/'
-	STATICFILES_DIRS = ( os.path.join(os.path.dirname(os.path.abspath(__file__)), 'static'), )
-	#STATICFILES_DIRS = ()
+    # STATIC_ROOT = '/app/griddl/static'
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    STATIC_URL = '/static/'
+    STATICFILES_DIRS = (os.path.join(os.path.dirname(
+                        os.path.abspath(__file__)), 'static'), )
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
-# List of finder classes that know how to find static files in various locations.
 STATICFILES_FINDERS = (
-	'django.contrib.staticfiles.finders.FileSystemFinder',
-	'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-#    'django.contrib.staticfiles.finders.DefaultStorageFinder',
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    #    'django.contrib.staticfiles.finders.DefaultStorageFinder',
 )
 
 # Make this unique, and don't share it with anybody.
@@ -140,25 +140,25 @@ SECRET_KEY = 'slvy^mosip%vo8atr69t)g$=vyhtmqggm8^w7#*e$_&^mj1261'
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
-	'django.template.loaders.filesystem.Loader',
-	'django.template.loaders.app_directories.Loader',
-#     'django.template.loaders.eggs.Loader',
+    'django.template.loaders.filesystem.Loader',
+    'django.template.loaders.app_directories.Loader',
+    #     'django.template.loaders.eggs.Loader',
 )
 
 TEMPLATE_DIRS = (
-	# Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-	# Always use forward slashes, even on Windows.
-	# Don't forget to use absolute paths, not relative paths.
+    # Put strings here, like "/home/html/django_templates"
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
 )
 
 MIDDLEWARE_CLASSES = (
-	'django.middleware.common.CommonMiddleware',
-	'django.contrib.sessions.middleware.SessionMiddleware',
-	'django.middleware.csrf.CsrfViewMiddleware',
-	'django.contrib.auth.middleware.AuthenticationMiddleware',
-	'django.contrib.messages.middleware.MessageMiddleware',
-	# Uncomment the next line for simple clickjacking protection:
-	# 'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    # Uncomment the next line for simple clickjacking protection:
+    # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
 ROOT_URLCONF = 'mysite.urls'
@@ -167,99 +167,107 @@ ROOT_URLCONF = 'mysite.urls'
 WSGI_APPLICATION = 'mysite.wsgi.application'
 
 INSTALLED_APPS = (
-	'django.contrib.auth',
-	'django.contrib.contenttypes',
-	'django.contrib.sessions',
-	'django.contrib.sites',
-	'django.contrib.messages',
-	
-	'django.contrib.staticfiles', # enable static files
-	'django.contrib.admin', # enable admin
-	# 'django.contrib.admindocs', # enable admin documentation
-	
-	'griddl',
-  'billing'
-#	'south',
-	#'rest_framework'
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.sites',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',  # enable static files
+    'django.contrib.admin',  # enable admin
+    # 'django.contrib.admindocs', # enable admin documentation
+
+    'bootstrap3',  # django-bootstrap3
+    'crispy_forms',
+
+    'griddl',
+    'billing'
+    #    'south',
+    #  'rest_framework'
 )
+
+# crispy_forms setting
+CRISPY_TEMPLATE_PACK = 'bootstrap3'
 
 SESSION_SERIALIZER = 'django.contrib.sessions.serializers.JSONSerializer'
 
-#REST_FRAMEWORK = {
-#	# Use hyperlinked styles by default.
-#	# Only used if the `serializer_class` attribute is not set on a view.
-#	'DEFAULT_MODEL_SERIALIZER_CLASS':
-#		'rest_framework.serializers.HyperlinkedModelSerializer',
-#	
-#	# Use Django's standard `django.contrib.auth` permissions,
-#	# or allow read-only access for unauthenticated users.
-#	'DEFAULT_PERMISSION_CLASSES': [
-#		'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-#	]
-#}
+# REST_FRAMEWORK = {
+#    # Use hyperlinked styles by default.
+#    # Only used if the `serializer_class` attribute is not set on a view.
+#    'DEFAULT_MODEL_SERIALIZER_CLASS':
+#        'rest_framework.serializers.HyperlinkedModelSerializer',
+#
+#    # Use Django's standard `django.contrib.auth` permissions,
+#    # or allow read-only access for unauthenticated users.
+#    'DEFAULT_PERMISSION_CLASSES': [
+#        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+#    ]
+# }
 
-# See http://docs.djangoproject.com/en/dev/topics/logging for more details on how to customize your logging configuration.
+# See http://docs.djangoproject.com/en/dev/topics/logging for more details
+# on how to customize your logging configuration.
 
-# this is quickly going to force us to adopt a mutiple-settings-file approach or risk madness, maybe both
+# this is quickly going to force us to adopt a mutiple-settings-file approach
+# or risk madness, maybe both
 if noahDev or developmentServer:
     default_handler = 'file'
 else:
     default_handler = 'stdout'
 
 LOGGING = {
-	'version': 1,
-	'disable_existing_loggers': False,
-	'filters': {
-		'require_debug_false': {
-			'()': 'django.utils.log.RequireDebugFalse'
-		}
-	},
-        'formatters': {
-                'verbose': {
-                    'format' : "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
-                    'datefmt' : "%d/%b/%Y %H:%M:%S"
-                },
-                'simple': {
-                    'format': '%(levelname)s - %(message)s'
-                },
+    'version': 1,
+    'disable_existing_loggers': False,
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse'
+        }
+    },
+    'formatters': {
+        'verbose': {
+            'format': "[%(asctime)s] %(levelname)s \
+                       [%(name)s:%(lineno)s] %(message)s",
+            'datefmt': "%d/%b/%Y %H:%M:%S"
         },
-	'handlers': {
-		'mail_admins': {
-			'level': 'ERROR',
-			'filters': ['require_debug_false'],
-			'class': 'django.utils.log.AdminEmailHandler'
-		},
-    'file': {
-        'level': 'DEBUG',
-        'class': 'logging.FileHandler',
-        'filename': BASE_DIR + '/app.log',
-        'formatter': 'verbose'
+        'simple': {
+            'format': '%(levelname)s - %(message)s'
+        },
     },
-    'stdout': {
-        'level': 'DEBUG',
-        'class': 'logging.StreamHandler',
-        'formatter': 'verbose',
+    'handlers': {
+        'mail_admins': {
+            'level': 'ERROR',
+            'filters': ['require_debug_false'],
+            'class': 'django.utils.log.AdminEmailHandler'
+        },
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': BASE_DIR + '/app.log',
+            'formatter': 'verbose'
+        },
+        'stdout': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose',
 
-    }
-  },
-  'loggers': {
-    'django.request': {
-      'handlers': ['mail_admins'],
-      'level': 'ERROR',
-      'propagate': True,
+        }
     },
-    'billing': {
-        'handlers': [default_handler],
-        'level': 'DEBUG',
-        'propagate': False,
+    'loggers': {
+        'django.request': {
+            'handlers': ['mail_admins'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+        'billing': {
+            'handlers': [default_handler],
+            'level': 'DEBUG',
+            'propagate': False,
+        }
     }
-	}
 }
 
 API_CREDENTIALS = {
-        'fastspring': {
-                    'company': 'adamchmelynski',
-                    'login': 'adam.chmelynski+fsapi@gmail.com',
-                    'password':  'CAssIce548L4'
-        }
+    'fastspring': {
+        'company': 'adamchmelynski',
+        'login': 'adam.chmelynski+fsapi@gmail.com',
+        'password':  'CAssIce548L4'
+    }
 }
