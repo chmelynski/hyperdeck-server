e4c380d75bd5e68f095812746d632654beb00132
chmelynski <adam.chmelynski@gmail.com>
2016-03-31 08:56:15 -0400

    upload other iframe files

---
+++ featuretests/server.js
@@ -0,0 +1,13 @@
+
+var fs = require('fs');
+var express = require('express');
+var app = express();
+
+app.use(express.static('C:\\users\\adam\\desktop\\everything\\featuretests\\sandbox'));
+
+var server = app.listen(3000, function () {
+	var host = server.address().address;
+	var port = server.address().port;
+	console.log('Example app listening at http://%s:%s', host, port);
+});
+
