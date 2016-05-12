ab5394fd60098d8ff10b4e7578412282768aa2f1
chmelynski <adam.chmelynski@gmail.com>
2016-05-11 20:43:30 -0400

    add Upload function

--- griddl/static/griddl/js/griddl.components.js
+++ griddl/static/griddl/js/griddl.components.js
@@ -986,6 +986,57 @@ var Hide = Components.Hide = function(obj) {
 var ShowAll = Components.ShowAll = function() { Griddl.Core.objs.forEach(function(obj) { Show(obj); }); };
 var HideAll = Components.HideAll = function() { Griddl.Core.objs.forEach(function(obj) { Hide(obj); }); };
 
+function Upload() {
+	
+	// interface required:
+	//  obj.setArrayBuffer
+	//    OR
+	//  obj.setText
+	
+	// and optionally obj.setExt to set the extension (useful for images and fonts, for instance)
+	
+	// we also want drag-n-drop
+	
+	var obj = this;
+	
+	var fileChooser = $(document.createElement('input'));
+	fileChooser.attr('type', 'file');
+	
+	fileChooser.on('change', function() {
+		
+		var fileReader = new FileReader();
+		
+		fileReader.onload = function(event)
+		{
+			if (obj.setArrayBuffer)
+			{
+				obj.setArrayBuffer(event.target.result);
+			}
+			else if (obj.setText)
+			{
+				obj.setText(event.target.result);
+			}
+		};
+		
+		if (fileChooser[0].files.length > 0)
+		{
+			var f = fileChooser[0].files[0];
+			
+			if (obj.setExt) { obj.setExt(f.name.substring(f.name.lastIndexOf('.') + 1)); }
+			
+			if (obj.setArrayBuffer)
+			{
+				fileReader.readAsArrayBuffer(f);
+			}
+			else if (obj.setText)
+			{
+				fileReader.readAsText(f);
+			}
+		}
+	});
+	
+	fileChooser.click();
+}
 
 // all references to Griddl.Core.objs are collected here, in case we want to move objs to some other place
 function AddObj(obj) { Griddl.Core.objs[obj.name] = obj; Griddl.Core.objs.push(obj); MakeSortable(); }
