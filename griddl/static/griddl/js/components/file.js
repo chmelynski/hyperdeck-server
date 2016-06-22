69cca6faac70976a1755ff5b8e701011f1ed48a4
chmelynski <adam.chmelynski@gmail.com>
2016-06-22 17:22:14 -0400

    update file.js

--- griddl/static/griddl/js/components/file.js
+++ griddl/static/griddl/js/components/file.js
@@ -1,8 +1,6 @@
 
 (function() {
 
-// should imgfiles add to #output automatically?
-
 var File = function(json, type) {
 	
 	if (!json)
@@ -41,9 +39,17 @@ var File = function(json, type) {
 	this.span = null;
 	
 	this._b64 = json.data;
-	this._uint8array = Griddl.Components.Base64StringToUint8Array(this._b64.substr(this._b64.indexOf(','))); // data:application/binary;base64,
+	this._uint8array = Base64StringToUint8Array(this._b64.substr(this._b64.indexOf(','))); // data:application/binary;base64,
 	
+	this.filenameControl = null;
 	this._filename = json.filename;
+	
+	//this.ext = this._filename.substr(this._filename.lastIndexOf('.') + 1);
+	Object.defineProperty(this, 'ext', { 
+		get : function() {
+			this._filename.substr(this._filename.lastIndexOf('.') + 1);
+		},
+	});
 	
 	Object.defineProperty(this, 'filename', { 
 		get : function() {
@@ -62,7 +68,7 @@ var File = function(json, type) {
 		},
 		set : function(value) {
 			this._b64 = value;
-			this._uint8array = Griddl.Components.Base64StringToUint8Array(this._b64.substr(this._b64.indexOf(','))); // data:application/binary;base64,
+			this._uint8array = Base64StringToUint8Array(this._b64.substr(this._b64.indexOf(','))); // data:application/binary;base64,
 			this.add();
 			if (!Griddl.dirty) { Griddl.Components.MarkDirty(); }
 		}
@@ -74,7 +80,7 @@ var File = function(json, type) {
 		},
 		set : function(value) {
 			this._uint8array = value;
-			this._b64 = 'data:text/plain;base64,' + Griddl.Components.Uint8ArrayToBase64String(this._uint8array);
+			this._b64 = 'data:text/plain;base64,' + Uint8ArrayToBase64String(this._uint8array);
 			this.add();
 			if (!Griddl.dirty) { Griddl.Components.MarkDirty(); }
 		}
@@ -110,9 +116,15 @@ File.prototype.add = function() {
 		var imageElement = $('<img src="' + this.b64 + '"></img>');
 		imgdiv.append(imageElement);
 		
+		var blob = new Blob([this._uint8array], {type: 'image/' + this.ext});
+		var url = URL.createObjectURL(blob);
+		var urlDiv = $('<div style="margin:1em"></div>');
+		urlDiv.text('url: ' + url);
+		
 		var dimensionDiv = $('<div style="margin:1em"></div>');
 		dimensionDiv.text(imageElement[0].width + ' x ' + imageElement[0].height);
 		
+		this.div.append(urlDiv);
 		this.div.append(dimensionDiv);
 		this.div.append(imgdiv);
 	}
@@ -133,7 +145,7 @@ File.prototype.write = function() {
 };
 File.prototype.upload = function() {
 	
-	var obj = this;
+	var comp = this;
 	
 	var fileChooser = $(document.createElement('input'));
 	fileChooser.attr('type', 'file');
@@ -144,13 +156,13 @@ File.prototype.upload = function() {
 		
 		fileReader.onload = function(event)
 		{
-			this.uint8array = new Uint8Array(arrayBuffer);
+			comp.uint8array = new Uint8Array(event.target.result);
 		};
 		
 		if (fileChooser[0].files.length > 0)
 		{
 			var f = fileChooser[0].files[0];
-			obj.filename = f.name;
+			comp.filename = f.name;
 			fileReader.readAsArrayBuffer(f);
 		}
 	});
@@ -158,6 +170,7 @@ File.prototype.upload = function() {
 	fileChooser.click();
 };
 File.prototype.download = function() {
+	
 	var a = document.createElement('a');
 	a.href = this.b64;
 	
@@ -180,6 +193,60 @@ File.prototype.getText = function() { return this.b64; };
 File.prototype.setText = function(text) { this.b64 = text; };
 File.prototype.getData = function() { return this.uint8array; };
 File.prototype.setData = function(data) { this.uint8array = data; };
+
+function Base64StringToUint8Array(str) {
+	
+	function b64ToUint6(n) { return n>64&&n<91?n-65:n>96&&n<123?n-71:n>47&&n<58?n+4:n===43?62:n===47?63:0;}
+	
+	var nBlocksSize = 3;
+	var sB64Enc = str.replace(/[^A-Za-z0-9\+\/]/g, ""); // remove all non-eligible characters from the string
+	var nInLen = sB64Enc.length;
+	var nOutLen = nBlocksSize ? Math.ceil((nInLen * 3 + 1 >> 2) / nBlocksSize) * nBlocksSize : nInLen * 3 + 1 >> 2;
+	var taBytes = new Uint8Array(nOutLen);
+	
+	for (var nMod3, nMod4, nUint24 = 0, nOutIdx = 0, nInIdx = 0; nInIdx < nInLen; nInIdx++)
+	{
+		nMod4 = nInIdx & 3;
+		nUint24 |= b64ToUint6(sB64Enc.charCodeAt(nInIdx)) << 18 - 6 * nMod4;
+		
+		if (nMod4 === 3 || nInLen - nInIdx === 1)
+		{
+			for (nMod3 = 0; nMod3 < 3 && nOutIdx < nOutLen; nMod3++, nOutIdx++)
+			{
+				taBytes[nOutIdx] = nUint24 >>> (16 >>> nMod3 & 24) & 255;
+			}
+			
+			nUint24 = 0;
+		}
+	}
+	
+	return taBytes;
+}
+function Uint8ArrayToBase64String(uint8array) {
+	var nMod3 = '';
+	var sB64Enc = '';
+	
+	function uint6ToB64(n) { return n<26?n+65:n<52?n+71:n<62?n-4:n===62?43:n===63?47:65;}
+	
+	for (var nLen = uint8array.length, nUint24 = 0, nIdx = 0; nIdx < nLen; nIdx++)
+	{
+		nMod3 = nIdx % 3;
+		//if (nIdx > 0 && (nIdx * 4 / 3) % 76 === 0) { sB64Enc += "\r\n"; }
+		nUint24 |= uint8array[nIdx] << (16 >>> nMod3 & 24);
+		
+		if (nMod3 === 2 || uint8array.length - nIdx === 1)
+		{
+			var a = uint6ToB64(nUint24 >>> 18 & 63);
+			var b = uint6ToB64(nUint24 >>> 12 & 63);
+			var c = uint6ToB64(nUint24 >>>  6 & 63);
+			var d = uint6ToB64(nUint24 >>>  0 & 63);
+			sB64Enc += String.fromCharCode(a, b, c, d);
+			nUint24 = 0;
+		}
+	}
+	
+	return sB64Enc.replace(/A(?=A$|$)/g, "=");
+}
 
 Griddl.Components.file = File;
 Griddl.Components.jsfile = File;
