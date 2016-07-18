278608cff655849a856e8d850c6c7ee455180f75
chmelynski <adam.chmelynski@gmail.com>
2016-07-18 10:42:26 -0400

    s/Griddl/Hyperdeck

--- griddl/static/griddl/js/results.js
+++ griddl/static/griddl/js/results.js
@@ -2,7 +2,7 @@ window.addEventListener('message', receiveMessage, false);
 
 var playground = window.location.protocol + "//";
 chunks = window.location.hostname.split('.');
-if (chunks[0] == "griddl") {
+if (chunks[0] == "Hyperdeck") {
   chunks[0] = "www";
 } else {
   chunks[0] = chunks[0].slice(7);
@@ -46,7 +46,7 @@ $(document).ready(function() {
     save().done(function(success) {
       if (success) {
         $.alert('Your workbook has been saved.', 'success');
-        Griddl.Components.MarkClean();
+        Hyperdeck.Components.MarkClean();
       }
     });
   }); 
@@ -62,7 +62,7 @@ $(document).ready(function() {
   });
 
   $('#fileChooser').on('change', function(event) {
-    Griddl.IO.HandleLocalLoad(event.target.files);
+    Hyperdeck.IO.HandleLocalLoad(event.target.files);
   });
 
 });
@@ -97,7 +97,7 @@ function receiveMessage(event) {
   if (data.action) {
     switch (data.action) {
       case 'load':
-        Griddl.Components.Main(data.text);
+        Hyperdeck.Components.Main(data.text);
         break;
       case 'resolve':
         console.log(data);
@@ -113,7 +113,7 @@ function receiveMessage(event) {
 }
 
 function save() {
-  text = Griddl.Components.SaveToText();
+  text = Hyperdeck.Components.SaveToText();
   var saveResult = window.saveResult = $.Deferred();
   parent.postMessage({'action': 'save', 'text': text, 'deferred': 'saveResult'}, playground);
 
@@ -121,7 +121,7 @@ function save() {
 }
 
 function save_as(newname) {
-  text = Griddl.Components.SaveToText();
+  text = Hyperdeck.Components.SaveToText();
   var saveAsResult = window.saveAsResult = $.Deferred();
 
   parent.postMessage({'action': 'save_as', 'text': text, 'newname': newname, 'deferred': 'saveAsResult'}, playground);
