743f6c0906f4af96a9eff705d8e1206ff815f857
Noah Hall <noah.t.hall@gmail.com>
2016-07-28 17:13:30 +0000

    WIP/partial on billing system tests

--- billing/tests.py
+++ billing/tests.py
@@ -1,3 +1,62 @@
-from django.test import TestCase
+from django.test import RequestFactory, TestCase
+from django.utils import timezone
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+import mock
+import requests
+
+from mysite import settings
+from .models import BillingRedirect, Subscription
+
+
+class BaseTest(TestCase):
+    '''
+    Create a clean test user and a request, and cleanup after testing.
+    '''
+    def setUp(self):
+        self.user = get_user_model().objects \
+            .create_user("test_user", "test@hyperdeck.io", "blerp")
+        self.request = RequestFactory()
+        self.request.session = mock.MagicMock()
+        self.request.user = self.user
+
+    def tearDown(self):
+        self.user.delete()
+
+
+class TestRedirects(BaseTest):
+    '''
+    Test creation and resolution of BillingRedirects
+    '''
+    def test_create(self):
+        redirect = BillingRedirect.create(accountid=self.user.account.pk,
+                                          planid=1, created=timezone.now())
+        redirect.save()
+        self.assertEqual(redirect.status, 0)
+
+    def test_redirect(self):
+        redirect = BillingRedirect.create(accountid=self.user.account.pk,
+                                          planid=1, created=timezone.now())
+        redirect.save()
+        response = requests.get(redirect.url)
+        print response.history
+        self.assertEqual(response.history[0].status_code, 301)
+        self.assertIn('fastspring.com', response.url)
+        self.assertEqual(redirect.status, 1)
+
+
+class TestSubscriptions(BaseTest):
+    '''
+    Test all stages of subscription lifecycle with mock FastSpring messages:
+        - creation
+        - upgrade
+        - downgrade
+    '''
+    def test_create(self):
+        pass
+
+    def test_upgrade(self):
+        pass
+
+    def test_downgrade(self):
+        pass
