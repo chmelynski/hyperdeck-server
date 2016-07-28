594e223270ff888afb7dd9cdf8c374b366dd2bb0
Noah Hall <noah.t.hall@gmail.com>
2016-07-28 20:41:09 +0000

    allow underscores in dir/file names

--- griddl/urls.py
+++ griddl/urls.py
@@ -29,9 +29,9 @@ urlpatterns = patterns(
     url(r'^move$', views.move),
     url(r'^togglepublic$', views.togglepublic),
 
-    url(r'^f/(?P<userid>[0-9]+)/?(?P<path>[A-Za-z0-9-/ %]*)/(?P<slug>[A-Za-z0-9-/ %]*)$', views.workbook),
-    url(r'^d/(?P<userid>[0-9]+)/?(?P<path>[A-Za-z0-9-/ ]*)$', views.directory),
-    url(r'^results/f/(?P<userid>[0-9]+)/?(?P<path>[A-Za-z0-9-/ %]*)/(?P<slug>[A-Za-z0-9-/ %]*)$', views.results),
+    url(r'^f/(?P<userid>[0-9]+)/?(?P<path>[A-Za-z0-9-/ _%]*)/(?P<slug>[A-Za-z0-9-/ _%]*)$', views.workbook),
+    url(r'^d/(?P<userid>[0-9]+)/?(?P<path>[A-Za-z0-9-/ _]*)$', views.directory),
+    url(r'^results/f/(?P<userid>[0-9]+)/?(?P<path>[A-Za-z0-9-/ _%]*)/(?P<slug>[A-Za-z0-9-/ _%]*)$', views.results),
 )
 
 # builtin auth views
