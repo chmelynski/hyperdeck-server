2bd98138bd836324a69d04956b18e2e5d654553d
chmelynski <adam.chmelynski@gmail.com>
2016-08-02 11:24:35 -0400

    some admin changes

--- griddl/admin.py
+++ griddl/admin.py
@@ -4,19 +4,32 @@ from django.contrib.auth.models import User
 
 from models import Account, DefaultWorkbook, Workbook
 
+# https://docs.djangoproject.com/en/1.9/ref/contrib/admin/#django.contrib.admin.ModelAdmin.exclude
+# https://docs.djangoproject.com/en/1.9/ref/contrib/admin/#django.contrib.admin.ModelAdmin.list_display
+# https://docs.djangoproject.com/en/1.9/ref/contrib/admin/#django.contrib.admin.ModelAdmin.list_editable
+# https://docs.djangoproject.com/en/1.9/ref/contrib/admin/#django.contrib.admin.ModelAdmin.list_filter
+# https://docs.djangoproject.com/en/1.9/ref/contrib/admin/#django.contrib.admin.ModelAdmin.ordering
+# https://docs.djangoproject.com/en/1.9/ref/contrib/admin/#django.contrib.admin.ModelAdmin.readonly_fields
 
 class AccountInline(admin.StackedInline):
     model = Account
     can_delete = False
     readonly_fields = ('size',)
 
-
 class UserAdmin(UserAdmin):
     inlines = (AccountInline,)
+    list_display = ('username','email_address','Plan','Size')
+    
+    def Plan(self, obj):
+        return obj.account.plan
 
+    def Size(self, obj):
+        return obj.account.size
 
 class WorkbookAdmin(admin.ModelAdmin):
     readonly_fields = ('size',)
+    exclude = ('text',)
+    list_display = ('owner','slug','size')
 
 
 admin.site.unregister(User)
