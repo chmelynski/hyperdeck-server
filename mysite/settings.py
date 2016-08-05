5b5dd76b81002251f9fccc428f247a87f09b3754
Noah Hall <noah.t.hall@gmail.com>
2016-08-05 20:16:44 +0000

    maybe this fixes it tho

--- mysite/settings.py
+++ mysite/settings.py
@@ -311,9 +311,8 @@ DEBUG_TOOLBAR_PATCH_SETTINGS = False
 INTERNAL_IPS = ['127.0.0.1', '24.177.237.106', '192.241.210.119']
 
 # subdomain settings
-REMOVE_WWW_FROM_DOMAIN = False
 SUBDOMAINS = {
-    'main': None,
+    'main': '',
     'sandbox': 'griddl'
 }
 
