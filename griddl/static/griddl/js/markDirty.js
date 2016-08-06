f39b54ca5316983b7d60e11b8b0e4a22fdae8148
chmelynski <adam.chmelynski@gmail.com>
2016-08-06 15:54:39 -0400

    comment out MarkDirty postMessage
    looks like concatenation did something to break the retrieval of 'playground' from window

--- griddl/static/griddl/js/markDirty.js
+++ griddl/static/griddl/js/markDirty.js
@@ -3,12 +3,11 @@
   var playground = window.playground;
 
   Hyperdeck.dirty = false;
-  var titleTag = document.getElementsByTagName('title')[0];
 
   Hyperdeck.Components.MarkDirty = function() {
     if (!Hyperdeck.dirty) {
       Hyperdeck.dirty = true;
-      parent.postMessage({'action': 'markDirty'}, playground);
+      //parent.postMessage({'action': 'markDirty'}, playground);
       $('#saveMenuButton').addClass("bg-danger");
     }
   };
@@ -16,7 +15,7 @@
   Hyperdeck.Components.MarkClean = function() {
     if (Hyperdeck.dirty) {
       Hyperdeck.dirty = false;
-      parent.postMessage({'action': 'markClean'}, playground);
+      //parent.postMessage({'action': 'markClean'}, playground);
       $('#saveMenuButton').removeClass("bg-danger");
     }
   };
