ad8c28df6f60d9a65cd0eedd724c3b64fd60292b
chmelynski <adam.chmelynski@gmail.com>
2016-08-12 14:07:03 -0400

    add detail to billing admin

--- billing/admin.py
+++ billing/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
 
-from models import Subscription
+from models import Subscription, BillingRedirect
 
-admin.site.register(Subscription)
+class SubscriptionAdmin(admin.ModelAdmin):
+    list_display = ('reference_id', 'status', 'created', 'plan')
+    #list_display_links = ('reference_id',)
+    
+class BillingRedirectAdmin(admin.ModelAdmin):
+    list_display = ('created', 'account', 'plan', 'referrer', 'updated', 'status')
+    #list_display_links = ('slug',)
+
+admin.site.register(Subscription, SubscriptionAdmin)
+admin.site.register(BillingRedirect, BillingRedirectAdmin)
