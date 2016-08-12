ad35c5d03e213fb07c898515193ef8a13ce4f40b
chmelynski <adam.chmelynski@gmail.com>
2016-08-12 14:52:15 -0400

    rearrange billing admin display

--- billing/admin.py
+++ billing/admin.py
@@ -3,12 +3,12 @@ from django.contrib import admin
 from models import Subscription, BillingRedirect
 
 class SubscriptionAdmin(admin.ModelAdmin):
-    list_display = ('reference_id', 'status', 'created', 'plan')
+    list_display = ('reference_id', 'status', 'plan', 'created')
     #list_display_links = ('reference_id',)
     
 class BillingRedirectAdmin(admin.ModelAdmin):
-    list_display = ('created', 'account', 'plan', 'referrer', 'updated', 'status')
-    #list_display_links = ('slug',)
+    list_display = ('account', 'status', 'plan', 'referrer', 'created', 'updated')
+    #list_display_links = ('account',)
 
 admin.site.register(Subscription, SubscriptionAdmin)
 admin.site.register(BillingRedirect, BillingRedirectAdmin)
