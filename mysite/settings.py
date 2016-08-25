7067bdfd14a6e5f3fa04cd37b9b76601c2552d74
chmelynski <adam.chmelynski@gmail.com>
2016-08-25 11:42:03 -0400

    add allowed hosts
    * Update settings.py
    
    * Update settings.py
    
    * Update settings.py
    
    * Update settings.py
    
    * Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -20,8 +20,6 @@ if developmentServer or noahDev or staging or herokuDev:
 else:
     DEBUG = False
 
-DEBUG = True
-
 ADMINS = (
     ("Noah Hall", "noah.t.hall@gmail.com")
 )
@@ -68,7 +66,7 @@ else:
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['hyperdeck.io','workbook.hyperdeck.io','sandbox.hyperdeck.io']
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
@@ -347,13 +345,8 @@ SUBDOMAIN_URLCONFS = {
 SESSION_COOKIE_DOMAIN = ".hyperdeck.io"
 SESSION_COOKIE_NAME = SUBDOMAINS['main'] + 'sessionid'
 
-if DEBUG:
-    COMPRESS = False
-else:
-    COMPRESS = True
-
 PIPELINE = {
-    'PIPELINE_ENABLED': COMPRESS, # True = compress
+    'PIPELINE_ENABLED': not DEBUG, # True = compress
     'JS_COMPRESSOR': 'pipeline.compressors.uglifyjs.UglifyJSCompressor',
     'UGLIFYJS_BINARY': os.path.join(BASE_DIR, 'node_modules', '.bin', 'uglifyjs'),
     'UGLIFYJS_ARGUMENTS': '-m',
